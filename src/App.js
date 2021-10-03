92312b9eee38aa8b47989f33b39b28da005a716f
pescaru98 <mihai.pescaru@elysian-software.com>
2021-10-03 20:30:55 +0300

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import React from 'react';
+import HomePage from '../src/components/home/HomePage';
+import AboutPage from '../src/components/about/AboutPage';
+import Header from './components/common/Header'
+import PageNotFound from "./components/pagenotfound/PageNotFound";
+import CoursesPage from "./components/courses/CoursesPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <Header/>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route path="/about" component={AboutPage}/>
+                <Route path="/courses" component={CoursesPage}/>
+                <Route component={PageNotFound}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
